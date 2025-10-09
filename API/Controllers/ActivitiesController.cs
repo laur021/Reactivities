12ea4a2d95f63170eaff9877c59f14697364951e
laur021 <laurence.bacolor@gmail.com>
2025-10-09 11:37:39 +0800

    Create Activity
    Create Activity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,3 +1,4 @@
+using Application.Activities.Commands;
 using Application.Activities.Queries;
 using Domain;
 using MediatR;
@@ -16,6 +17,13 @@ public class ActivitiesController : BaseApiController
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        return await Mediator.Send(new GetActivityDetail.Query{Id = id});
+        return await Mediator.Send(new GetActivityDetail.Query { Id = id });
     }
+
+    [HttpPost]
+    public async Task<ActionResult<string>> CreateActivity(Activity activity)
+    {
+        return await Mediator.Send(new CreateActivity.Command { Activity = activity });
+    }
+    
 }
