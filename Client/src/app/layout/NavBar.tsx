f2c3c84a8901565d6707c77110058ba87d0bf552
laur021 <laurence.bacolor@gmail.com>
2025-10-22 11:57:55 +0800

    Refactor NavBar to use MenuItemLink component
    Replaced static MenuItem navigation links in NavBar with a reusable MenuItemLink component for improved consistency and maintainability. Added MenuItemLink to shared components to handle navigation and active styling.

--- Client/src/app/layout/NavBar.tsx
+++ Client/src/app/layout/NavBar.tsx
@@ -1,14 +1,7 @@
 import { Group } from "@mui/icons-material";
-import {
-  AppBar,
-  Box,
-  Button,
-  Container,
-  CssBaseline,
-  MenuItem,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import { AppBar, Box, Container, CssBaseline, MenuItem, Toolbar, Typography } from "@mui/material";
+import { NavLink } from "react-router";
+import MenuItemLink from "../../shared/components/MenuItemLink";
 
 export default function NavBar() {
   return (
@@ -22,7 +15,7 @@ export default function NavBar() {
           <Container maxWidth="xl">
             <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
               <Box>
-                <MenuItem sx={{ display: "flex", gap: 2 }}>
+                <MenuItem component={NavLink} to="/" sx={{ display: "flex", gap: 2 }}>
                   <Group fontSize="large" />
                   <Typography variant="h4" fontWeight="bold">
                     Reactivities
@@ -30,31 +23,10 @@ export default function NavBar() {
                 </MenuItem>
               </Box>
               <Box sx={{ display: "flex", justifyContent: "space-between", gap: 4 }}>
-                <MenuItem>
-                  <Typography
-                    sx={{ fontSize: "1.2rem", textTransform: "uppercase", fontWeight: "bold" }}
-                  >
-                    Activities
-                  </Typography>
-                </MenuItem>
-                <MenuItem>
-                  <Typography
-                    sx={{ fontSize: "1.2rem", textTransform: "uppercase", fontWeight: "bold" }}
-                  >
-                    About
-                  </Typography>
-                </MenuItem>
-                <MenuItem>
-                  <Typography
-                    sx={{ fontSize: "1.2rem", textTransform: "uppercase", fontWeight: "bold" }}
-                  >
-                    Contact
-                  </Typography>
-                </MenuItem>
+                <MenuItemLink to="/activities">Activities</MenuItemLink>
+                <MenuItemLink to="/createActivity">Create Activity</MenuItemLink>
               </Box>
-              <Button onClick={() => {}} size="large" variant="contained" color="warning">
-                Create Activity
-              </Button>
+              <MenuItem>User Menu</MenuItem>
             </Toolbar>
           </Container>
         </AppBar>
