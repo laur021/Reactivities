0dff3821c4c09d3fc075601c75f4571df4941312
laur021 <laurence.bacolor@gmail.com>
2025-10-28 15:33:25 +0800

    Using an alternative approach with MobX
    Replaced Observer with observer HOC in Counter component for improved MobX integration. Updated CounterStore to use makeAutoObservable for simpler and more concise observable and action setup.

--- Client/src/features/counter/Counter.tsx
+++ Client/src/features/counter/Counter.tsx
@@ -1,30 +1,31 @@
 import { Button, ButtonGroup, Typography } from "@mui/material";
-import { Observer } from "mobx-react-lite";
+import { observer } from "mobx-react-lite";
 import { useStore } from "../../lib/hooks/useStore";
 
-export default function Counter() {
+const Counter = observer(function Counter() {
   const { counterStore } = useStore();
   return (
     <>
-      <Observer>
-        {() => (
-          <>
-            <Typography variant="h4" gutterBottom>
-              {counterStore.title}
-            </Typography>
-            <Typography variant="h6" gutterBottom>
-              The count is: {counterStore.count}
-            </Typography>
-          </>
-        )}
-      </Observer>
+      <Typography variant="h4" gutterBottom>
+        {counterStore.title}
+      </Typography>
+      <Typography variant="h6" gutterBottom>
+        The count is: {counterStore.count}
+      </Typography>
+
       <ButtonGroup sx={{ mt: 3 }}>
         <Button onClick={() => counterStore.decrement()} variant="contained" color="error">
           Decrement
         </Button>
-        <Button onClick={() => counterStore.increment()} variant="contained" color="info" >increment</Button>
-        <Button onClick={() => counterStore.increment(5)} variant="contained" color="success" >increment by 5</Button>
+        <Button onClick={() => counterStore.increment()} variant="contained" color="info">
+          increment
+        </Button>
+        <Button onClick={() => counterStore.increment(5)} variant="contained" color="success">
+          increment by 5
+        </Button>
       </ButtonGroup>
     </>
   );
-}
+});
+
+export default Counter;
