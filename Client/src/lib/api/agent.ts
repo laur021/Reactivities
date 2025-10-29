dda6b99f650858b3a7606e222d859c12acf46f88
laur021 <laurence.bacolor@gmail.com>
2025-10-29 12:07:28 +0800

    React query fetching
    Introduced a 1-second artificial delay to all axios responses for improved UX during loading. Updated useActivities hook to enable activities query only on '/activities' route and when no ID is provided.

--- Client/src/lib/api/agent.ts
+++ Client/src/lib/api/agent.ts
@@ -1,6 +1,12 @@
 import axios from "axios";
 import { store } from "../stores/store";
 
+const sleep = (delay: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+};
+
 const agent = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
@@ -12,6 +18,7 @@ agent.interceptors.request.use((config) => {
 
 agent.interceptors.response.use(async (response) => {
   try {
+    await sleep(1000);
     return response;
   } catch (error) {
     console.log(error);
