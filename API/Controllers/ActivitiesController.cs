7a53acbb5effa1e3c7fa67fd47621109c66ddda5
laur021 <laurence.bacolor@gmail.com>
2025-10-31 09:47:56 +0800

    Refactor activity DTOs and validators for reuse
    Introduces BaseActivityDto to unify shared properties for activity creation and editing. Refactors CreateActivityDto and adds EditActivityDto, updating controller and command logic to use these DTOs. Adds BaseActivityValidator for shared validation rules, refactors CreateActivityValidator, and adds EditActivityValidator. Updates mapping profiles to support new DTOs.

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -19,8 +19,6 @@ public class ActivitiesController : BaseApiController
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        throw new Exception("Server test error");
-
         return HandleResult(await Mediator.Send(new GetActivityDetail.Query { Id = id }));
     }
 
@@ -31,9 +29,9 @@ public class ActivitiesController : BaseApiController
     }
 
     [HttpPut]
-    public async Task<ActionResult> EditActivity(Activity activity)
+    public async Task<ActionResult> EditActivity(EditActivityDto activity)
     {
-        return HandleResult(await Mediator.Send(new EditActivity.Command { Activity = activity }));
+        return HandleResult(await Mediator.Send(new EditActivity.Command { ActivityDto = activity }));
     }
 
     [HttpDelete("{id}")]
