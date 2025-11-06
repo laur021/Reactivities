12f3037fc0438ab20467f82ecd4631420b3ddde2
laur021 <laurence.bacolor@gmail.com>
2025-11-06 16:46:02 +0800

    update email error
    update email error

--- Client/src/lib/schemas/registerSchema.ts
+++ Client/src/lib/schemas/registerSchema.ts
@@ -2,7 +2,7 @@ import z from "zod";
 import { requiredString } from "../util/util";
 
 export const registerSchema = z.object({
-  email: z.email({ error: "Email is required" }),
+  email: z.email({ error: "Invalid Email" }),
   displayName: requiredString("Display name"),
   password: requiredString("Password"),
 });
